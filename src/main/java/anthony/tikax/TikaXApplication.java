c6edf932f89e8943bb094c4f05d2d6160536e4aa
yunqing <jzhang0703@163.com>
2025-12-03 23:06:27 +0800

    init

---
+++ src/main/java/anthony/tikax/TikaXApplication.java
@@ -0,0 +1,13 @@
+package anthony.tikax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikaXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikaXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anthony/tikax/TikaXApplicationTests.java
@@ -0,0 +1,13 @@
+package anthony.tikax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikaXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
