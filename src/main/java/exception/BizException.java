3bd187b911fd62fb9f24aacf7f92ba2dbb8f57fc
yunqing <jzhang0703@163.com>
2025-12-04 15:12:38 +0800

    feat(exception): BizException

---
+++ src/main/java/exception/BizException.java
@@ -0,0 +1,27 @@
+package exception;
+
+import lombok.Getter;
+
+@Getter
+public class BizException extends RuntimeException {
+
+    private final String code;
+
+    public BizException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BizException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public BizException(ErrorCode errorCode) {
+        this(String.valueOf(errorCode.getCode()), errorCode.getMessage());
+    }
+
+    public BizException(ErrorCode errorCode, Throwable cause) {
+        this(String.valueOf(errorCode.getCode()), errorCode.getMessage(), cause);
+    }
+}
---
+++ src/main/java/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    ;
+
+    private final int code;
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
